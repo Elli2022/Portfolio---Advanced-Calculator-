3d9748f39d696578269d7c4a0207f0ca99147cf1
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-03-28 10:39:35 +0200

    push

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,12 +1,12 @@
 const display = document.getElementById('display') as HTMLInputElement;
 const numberButtons = document.querySelectorAll('[data-number]') as NodeListOf<HTMLButtonElement>;
 const operatorButtons = document.querySelectorAll('[data-operator]') as NodeListOf<HTMLButtonElement>;
-const equalsButton = document.getElementById('equals') as HTMLButtonElement;
-const clearButton = document.getElementById('clear') as HTMLButtonElement;
+const equalsButton = document.querySelector('equals') as HTMLButtonElement;
+const clearButton = document.querySelector('clear') as HTMLButtonElement;
 
 let firstOperand: number | null=null;
 let secondOperand: number |null = null;
-let operator:string |null = null;
+let currentOperator:string |null = null;
 
 //Add event listeners for number buttons
 numberButtons.forEach(button => {
@@ -18,22 +18,51 @@ numberButtons.forEach(button => {
 //Add event listeners for operator buttons
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        display.value += button.textContent;
+        if(!firstOperand){
+            firstOperand = parseFloat(display.value);
+            currentOperator = button.textContent;
+            display.value += '';
+        }
+        
     });
 });
 
 //Add event listeners for equals button
 equalsButton.addEventListener('click', () => {
-    if(firstOperand!== null && secondOperand!== null && operator!== null) {
-        display.value = eval(display.value);
+    if(!firstOperand! ||  !currentOperator ) return;
+    secondOperand = parseFloat(display.value);
+    let result: number;
+    
+    switch(currentOperator){
+        case '+':
+            result = firstOperand + secondOperand;
+            break;
+
+        case '-':
+            result = firstOperand - secondOperand;
+            break;
+        case '*':
+            result = firstOperand * secondOperand;
+            break;
+
+        case '/':
+            result = firstOperand / secondOperand;
+            break;
+
+        default:
+            return;
     }
+    display.value = result.toString();
+    firstOperand = null;
+    secondOperand = null;
+    currentOperator = null;
 });
+
 
 //Add event listeners for clear button
 clearButton.addEventListener('click', () => {
     display.value = '';
     firstOperand = null;
     secondOperand = null;
-    operator = null;
+    currentOperator = null;
 });
-
